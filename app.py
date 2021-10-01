d0f754aa13c5713bcd1226bbf29fe61c8987f6b9
SHRADDHAPHALE <sphale28@gmail.com>
2021-10-01 08:49:24 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from books import app, db
+import os
 from flask import render_template, redirect, url_for
 from books.forms import AddForm, DelForm
 from books.models import Book
@@ -45,4 +46,5 @@ def delete():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
